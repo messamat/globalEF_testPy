bebfb46090dbd69594e82b288d381b3cb7139e92
messamat <messamat@uw.edu>
2022-12-07 06:50:18 -0500

    Download isimp2b

---
+++ download_isimp2b.py
@@ -0,0 +1,45 @@
+import os
+from pathlib import Path
+import requests
+
+
+rootdir = os.path.dirname(os.path.abspath(__file__)).split('\\src')[0]
+datdir = os.path.join(rootdir, 'data')
+resdir = os.path.join(rootdir, 'results')
+
+#hydrological output directory
+isimp2b_datdir = Path(datdir, 'isimp2b')
+if not isimp2b_datdir.exists():
+    isimp2b_datdir.mkdir()
+
+#Create list of URLs to get from API
+
+root_url = "https://files.isimip.org//ISIMIP2b//OutputData//water_global"
+ghm_list = ["H08", "LPJmL", "PCR-GLOBWB", "WaterGAP2-2c"]
+gcm_list = ["gfdl-esm2m", "hadgem2-es", "ipsl-cm5a-lr", "miroc5"]
+middle_url = "_ewembi_picontrol_1860soc_co2_dis_global_daily_"
+years = [f"{i}_{i+9}" for i in range(1661, 1861, 10)]
+
+isimp2b_pathlist = []
+for ghm in ghm_list:
+    for gcm in gcm_list:
+        for y in years:
+            isimp2b_pathlist.append(
+                "//".join([
+                    root_url, ghm, gcm, "pre-industrial",
+                    f"{ghm.lower()}_{gcm.lower()}{middle_url}{y}.nc4"
+                ])
+            )
+
+#Download nc files (need to improve exception handling)
+for url in isimp2b_pathlist:
+    outnc = Path(isimp2b_datdir, Path(url).name)
+    with open(outnc, "wb") as file:
+        # get request
+        print(f"Downloading {Path(url).name}")
+        response = requests.get(url)
+        file.write(response.content)
+
+
+
+
