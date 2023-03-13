MAJOR DEBUGGING: increase rounding precision for computing runoff, shift from total runoff to only surface runoff (negative values in total runoff), correct major issue with shift parameter in for loop (in preprocess_GLOBWB.py)

EF_utils.py
@@ -102,8 +102,10 @@ def compute_xrfdc(in_xr, quant_list, vname = 'dis'):
 # cell = run_fdc_merge.sel(lat=69.75, lon=-169.25) #Example with only no data (in the sea)
 # cell = run_fdc_merge.sel(lat=45.75, lon=5.25) #Example with data
 # cell = run_fdc_merge.sel(lat=25.25, lon=22.25) #Example with 0s
-def compute_smakhtinef_ts(cell, n_shift=1, loginterp_padding = 0.00001, vname = 'dis'):
-    fdc = np.round(cell.fdc_dis.values.squeeze() + loginterp_padding, 5)
+
+#cell = run_fdc_merge_disk.isel(longitude=350, latitude=60)
+def compute_smakhtinef_ts(cell, n_shift=1, loginterp_padding = 0.00001, vname = 'dis', round=6):
+    fdc = np.round(cell.fdc_dis.values.squeeze() + loginterp_padding, round)
     maxval = cell[vname].values.max()
 
     if (len(np.unique(fdc[~np.isnan(fdc)]))-n_shift) > 1: #If enough unique values in the FDC to interpolate after shifting
@@ -191,20 +193,25 @@ def compute_smakhtinef_stats(in_xr, out_dir, out_efnc_basename, n_shift=1, vname
     print("Compute e-flow values for each cell")
     smakhtinef = run_fdc_merge_disk.stack(gridcell=[lat_dimname, lon_dimname]).\
         groupby("gridcell").\
-        map(compute_smakhtinef_ts, args=[n_shift, 0.00001, vname]).\
+        map(compute_smakhtinef_ts, args=[n_shift, 0.000001, vname, 6]).\
         unstack('gridcell')
     smakhtinef['time'] = run_fdc_merge_disk.time
+    smakhtinef.to_netcdf(Path(out_dir, f'smakthin_ef_{["a", "b", "c", "d"][n_shift-1]}_test.nc4'))
 
     #Compute e-flow relative to MAF
     print("Compute e-flow relative to MAF")
     run_maf = run_fdc_merge_disk[vname].mean(dim='time') #Mean Flow
     smakhtinef_mean = xr.merge([run_maf,
-                                  xr.Dataset(dict(ef_a_mean = smakhtinef.mean(dim='time').round(5)))])
-    smakhtinef_relative = xr.where((smakhtinef_mean[vname].round(decimals=5) > 0) | (np.isnan(smakhtinef_mean[vname])),
-                                     (smakhtinef_mean.ef_a_mean/smakhtinef_mean[vname].round(decimals=5)).values, 1)
+                                  xr.Dataset(dict(ef_mean = smakhtinef.mean(dim='time')))])
+    smakhtinef_relative = xr.where((smakhtinef_mean[vname].round(decimals=6) > 0) | (np.isnan(smakhtinef_mean[vname])),
+                                     (smakhtinef_mean.ef_mean.round(decimals=6)/
+                                      smakhtinef_mean[vname].round(decimals=6)).values, 1)
+    #smakhtinef_relative.to_netcdf(Path(out_dir, 'smakthinef_relative.nc4'))
+
     #Compute total annual e-flow
     print("Compute total annual e-flow")
     smakhtinef_taef = smakhtinef.groupby('month').mean(skipna=True).sum(dim='month', skipna=False)
+    #smakhtinef_taef.to_netcdf(Path(out_dir, 'smakhtinef_taef.nc4'))
 
     #Merge and write out
     print("Merge and write-out")

preprocess_GLOBWB.py
@@ -1,3 +1,5 @@
+import re
+
 from globalEF_comparison_setup import * #See this module for directory structure
 #import time
 import xarray as xr
@@ -15,10 +17,49 @@ if not globwb_resdir.exists():
 
 # Create a searchable dataframe of hydrological layers
 discharge_path = Path(globwb_datdir, 'discharge_monthAvg_output_1958-01-31_to_2015-12-31.zip.nc')
-runoff_path = Path(globwb_datdir, 'totalRunoff_monthTot_output_1958-01-31_to_2015-12-31.zip.nc')
+runoff_path = Path(globwb_datdir, 'runoff_monthTot_output_1958-01-31_to_2015-12-31.zip.nc')
 
-def preprocess_GLOBWB(in_path, in_var):
-    outpath_ef_notsmakhtin = Path(globwb_resdir, f'{in_path.name.split(".")[0]}_allefbutsmakhtin.nc4')
+
+def split_netcdf(in_netcdf, factor,  out_pathroot, lat_dimname='latitude', lon_dimname='longitude'):
+    if isinstance(in_netcdf, (str, Path)):
+        in_netcdf = xr.open_dataset(in_netcdf)
+
+
+    if len(np.unique(in_netcdf[lon_dimname].values[:-1] - in_netcdf[lon_dimname].values[1:])) > 1:
+        lat_binlist = list(range(0, in_netcdf.dims[lat_dimname],
+                                 int(np.ceil(in_netcdf.dims[lat_dimname]/factor))))
+        lat_binlist.append(in_netcdf.dims[lat_dimname])
+
+        lon_binlist = list(range(0, in_netcdf.dims[lon_dimname],
+                                 int(np.ceil(in_netcdf.dims[lon_dimname]/factor))))
+        lon_binlist.append(in_netcdf.dims[lon_dimname])
+
+        out_tilelist = []
+        for lat_min, lat_max in zip(lat_binlist[:-1], lat_binlist[1:]):
+            for lon_min, lon_max in zip(lon_binlist[:-1], lon_binlist[1:]):
+                print(f'Writing bbox: {lon_min}, {lat_min}, {lon_max}, {lat_max}')
+                out_divxr = Path(f'{out_pathroot}_{lon_min}_{lat_min}_{lon_max}_{lat_max}.nc4')
+
+                out_tilelist.append(out_divxr)
+
+                slice_dict = {lon_dimname: slice(lon_min, lon_max),
+                              lat_dimname: slice(lat_min, lat_max)}
+
+                if not Path(out_divxr).exists():
+                    in_netcdf.isel(slice_dict).to_netcdf(out_divxr)
+
+        return(out_tilelist)
+
+    else: #to continue
+        bbox = [min(in_netcdf[lon_dimname].values),
+                min(in_netcdf[lat_dimname].values),
+                max(in_netcdf[lon_dimname].values),
+                max(in_netcdf[lat_dimname].values)
+                ]
+
+
+def preprocess_GLOBWB(in_path, in_var, out_resdir):
+    outpath_ef_notsmakhtin = Path(out_resdir, f'{in_path.name.split(".")[0]}_allefbutsmakhtin.nc4')
 
     # dis_xr = spatiotemporal_chunk_optimized_acrosstime(
     #     xr.open_dataset(in_path),
@@ -42,26 +83,68 @@ def preprocess_GLOBWB(in_path, in_var):
     else:
         print(f"{outpath_ef_notsmakhtin.name} already exists. Skipping...")
 
-    for shift, emc in enumerate(['a', 'b', 'c', 'd']):
-        outpath_ef_smakhtin = f'{in_path.name.split(".")[0]}_smakhtinef_{emc}.nc4'
-        if not Path(globwb_resdir, outpath_ef_smakhtin).exists():
-            print(f"Processing {outpath_ef_smakhtin}")
+    #Divide the raster in four tiles to be processed separately
+    dis_xr_tilelist = split_netcdf(in_netcdf=dis_xr,
+                                   factor=10,
+                                   out_pathroot=Path(out_resdir, os.path.splitext(os.path.splitext(in_path.name)[0])[0]),
+                                   lat_dimname='latitude',
+                                   lon_dimname='longitude')
 
-            import time
-            start = time.time()
-            compute_smakhtinef_stats(in_xr = dis_xr,
-                                     out_dir = globwb_resdir,
-                                     vname=in_var,
-                                     out_efnc_basename=outpath_ef_smakhtin,
-                                     n_shift=shift,
-                                     lat_dimname='latitude',
-                                     lon_dimname='longitude',
-                                     scratch_file = 'scratch.nc4')
-            end = time.time()
-            print(end-start)
+    #For each tile
+    for tile_path in dis_xr_tilelist:
+        #For each EMC
+        for shift, emc in enumerate(['a', 'b', 'c', 'd']):
+            outpath_ef_smakhtin = f'{tile_path.name.split(".")[0]}_smakhtinef_{emc}.nc4'
+            if not Path(out_resdir, outpath_ef_smakhtin).exists():
+                print(f"Processing {outpath_ef_smakhtin}")
 
-        else:
-            print(f"{outpath_ef_smakhtin} already exists. Skipping...")
+                import time
+                start = time.time()
+                distile_xr = xr.open_dataset(tile_path)
 
-preprocess_GLOBWB(in_path=discharge_path, in_var='discharge')
-preprocess_GLOBWB(runoff_path, in_var='total_runoff')
+                compute_smakhtinef_stats(in_xr = distile_xr,
+                                         out_dir = out_resdir,
+                                         vname=in_var,
+                                         out_efnc_basename=outpath_ef_smakhtin,
+                                         n_shift=shift+1,
+                                         lat_dimname='latitude',
+                                         lon_dimname='longitude',
+                                         scratch_file = 'scratch.nc4')
+                end = time.time()
+                print(end-start)
+
+            else:
+                print(f"{outpath_ef_smakhtin} already exists. Skipping...")
+
+
+    full_outputpaths_list = [Path(globwb_resdir, f'{tile_path.name.split(".")[0]}_smakhtinef_{emc}.nc4'
+                                  ).exists()
+                             for tile_path in dis_xr_tilelist
+                             for emc in ['a', 'b', 'c', 'd']]
+    if all(full_outputpaths_list):
+        return(full_outputpaths_list)
+
+    #Re-merge the tiles
+
+smakthinef_dis_pathlist = preprocess_GLOBWB(in_path=discharge_path,
+                                            in_var='discharge',
+                                            out_resdir = globwb_resdir)
+smakthinef_qtot_pathlist = preprocess_GLOBWB(in_path=runoff_path,
+                                             in_var='total_runoff',
+                                             out_resdir = globwb_resdir)
+
+
+#Merge
+for var in ['discharge', 'runoff']:
+    for emc in ['a', 'b', 'c', 'd']:
+        tile_list = [p for p in globwb_resdir.iterdir() if
+                     re.match(re.compile(f"{var}_.*_smakhtinef_{emc}.nc4"), str(p.name))]
+
+        out_mergepath = Path(globwb_resdir,
+                             f"PCR_GLOBWB_{var}_smakthinef_{emc}.nc4")
+        #if not out_mergepath.exists():
+        print(f'EF smakthin {var}, {emc}: '
+              f'merging {len(list(tile_list))} tiles...')
+
+        mfxr = xr.combine_by_coords([xr.open_dataset(tile) for tile in tile_list])
+        mfxr.to_netcdf(out_mergepath)