5eddd2f4617334cfeab98108231015289886f661
messamat <messamat@uw.edu>
2023-04-25 08:05:08 -0400

    Update paths in link_sites_RiverATLAS.py

--- link_sites_RiverATLAS.py
+++ link_sites_RiverATLAS.py
@@ -2,26 +2,33 @@ from globalEF_comparison_setup import *
 
 #Intputs
 process_gdb = os.path.join(resdir, 'processing_outputs.gdb')
-EFpoints_0308_clean = os.path.join(process_gdb, 'EFpoints_20230308_clean')
+EFpoints_clean = os.path.join(process_gdb, 'EFpoints_20230424_clean')
 riveratlas = os.path.join(datdir, 'RiverATLAS_v10.gdb', 'RiverATLAS_v10')
 
 DA_grid = os.path.join(datdir, 'upstream_area_skm_15s.gdb', 'up_area_skm_15s') #HydroSHEDS upstream area grid
 
 #Outputs
 riveratlas_csv = os.path.join(resdir, 'RiverATLAS_v10tab.csv')
-EFpoints_0308_clean_riverjoin = os.path.join(process_gdb, 'EFpoints_20230308_clean_riverjoin')
+EFpoints_clean_riverjoin = os.path.join(process_gdb, 'EFpoints_20230424_clean_riverjoin')
+EFpoints_clean_riverjoin_csv = os.path.join(resdir, 'EFpoints_20230424_clean_riverjoin.csv')
+
 
 
 #------------------------------- Analysis ------------------------------------------------------------------------------
 #Join to RiverATLAS
-arcpy.analysis.SpatialJoin(EFpoints_0308_clean, riveratlas, EFpoints_0308_clean_riverjoin, join_operation='JOIN_ONE_TO_ONE',
+arcpy.analysis.SpatialJoin(EFpoints_clean, riveratlas, EFpoints_clean_riverjoin,
+                           join_operation='JOIN_ONE_TO_ONE',
                            join_type="KEEP_COMMON",
                            match_option='CLOSEST_GEODESIC',
                            distance_field_name='station_river_distance')
 
-#Extract HydroSHEDS and WaterGAP layers
-ExtractMultiValuesToPoints(in_point_features=EFpoints_0308_clean, in_rasters=DA_grid,
+#Extract HydroSHEDS
+ExtractMultiValuesToPoints(in_point_features=EFpoints_clean_riverjoin, in_rasters=DA_grid,
                            bilinear_interpolate_values='NONE')
+
+
+if not arcpy.Exists(EFpoints_clean_riverjoin_csv):
+    arcpy.management.CopyRows(in_rows = EFpoints_clean_riverjoin, out_table=EFpoints_clean_riverjoin_csv)
 
 #Export riveratlas attributes to csv file
 if not arcpy.Exists(riveratlas_csv):
